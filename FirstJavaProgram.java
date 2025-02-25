5e1cc835b82b2ee95e1143b5f8d0ade17afa6fb4
ankush1038 <103501353+ankush1038@users.noreply.github.com>
2025-02-25 14:17:22 +0530

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,6 +1,6 @@
 class FirstJavaProgram{
 public static void main(string[] args){
   // Changes
-system.out.print("Starting with Cpagemini training");
+system.out.print("Starting with Capagemini training");
 }
 }
